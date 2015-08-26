f856767034626923d7c730dd88f64e99c6ddd4d9
simonghales <simonghales@gmail.com>
2015-08-26 20:00:32 +1000

    fixed some of the resizable

--- js/storyapp/main.js
+++ js/storyapp/main.js
@@ -17,9 +17,35 @@ var toggleEditing = function() {
 }
 
 var enableEditing = function() {
-	console.log("Enable editing!");
 
-	$(".storyText__resizable").resizable();
+	// Story Containers
+	$(".storyContainer__resizable").resizable({
+		// maxWidth: "100%", get width of screen?
+		// containment: "parent",
+		handles: "e, w",
+		resize: function(event, ui) {
+			var newWidth = ui.originalSize.width+((ui.size.width - ui.originalSize.width)*2);
+	        $(this).width(newWidth).position({
+	            of: $(this).parent(),
+	            my: "center center",
+	            at: "center center"
+	        });
+	        $(this).css("left", "auto").css("right", "auto");
+	        // ui.size.width += (ui.size.width - ui.originalSize.width);
+	        //not knowing how you center your stuff i do it the jquery-ui way can be left out
+	        // $(this).position({
+	        //     of: $(this).parent(),
+	        //     my: "center center",
+	        //     at: "center center"
+	        // })
+	    }
+	});
+	$(".storyContainer__resizable").resizable("enable");
+
+	// Story Text Blocks
+	$(".storyText__resizable").resizable({
+		containment: "parent",
+	});
 	$(".storyText__resizable").resizable("enable");
 
 	siteStates.editing = true;
@@ -27,8 +53,11 @@ var enableEditing = function() {
 }
 
 var disableEditing = function() {
-	console.log("Disable editing!");
 
+	// Story Containers
+	$(".storyContainer__resizable").resizable("disable");
+
+	// Story Text Blocks
 	$(".storyText__resizable").resizable("disable");
 
 	siteStates.editing = false;
