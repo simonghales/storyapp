af6206e0270407424d80e20374967a75ae9ac0d7
simonghales <simonghales@gmail.com>
2015-09-28 22:10:30 +1000

    front-end image resizing

--- js/storyapp/controllers/cont-imageUpload.js
+++ js/storyapp/controllers/cont-imageUpload.js
@@ -1,190 +1,205 @@
-angular.module('storyApp').controller('ImageUpload', ['$scope', 'Upload', 'ImgurService', 'StoryService', function($scope, Upload, ImgurService, StoryService) {
+(function () {
+    'use strict';
 
-    console.log("Init imageUpload controller!");
+    angular
+        .module('storyApp')
+        .controller('ImageUpload', ImageUpload);
 
-    var imageUpload = this;
+    ImageUpload.$inject = ['$scope', 'Upload', 'ImgurService', 'StoryService'];
 
-    imageUpload.states = {
-        busy: false,
-        error: false,
-        uploading: false,
-        processing: false
-    }
-    imageUpload.uploadProgress = 0;
-    imageUpload.url = "";
-    imageUpload.image = "";
-    imageUpload.uploadedImage = null;
+    /* @ngInject */
+    function ImageUpload($scope, Upload, ImgurService, StoryService) {
+        /* jshint validthis: true */
+        var vm = this;
 
-    imageUpload.resizeImage = function(file) {
+        vm.activate = activate;
+        vm.resizeImage = resizeImage;
+        vm.uploadFile = uploadFile;
+        vm.uploadUrlCheck = uploadUrlCheck;
+        vm.uploadUrl = uploadUrl;
+        vm.updateImage = updateImage;
+        vm.confirm = confirm;
+        vm.cancel = cancel;
 
-        console.log("Provided file", file);
+        vm.states = {
+            busy: false,
+            error: false,
+            uploading: false,
+            processing: false
+        }
+        vm.uploadProgress = 0;
+        vm.url = "";
+        vm.image = "";
+        vm.uploadedImage = null;
 
-        var reader = new FileReader();
-        reader.onloadend = function() {
+        activate();
 
-            var tempImg = new Image();
-            tempImg.src = reader.result;
-            tempImg.onload = function() {
+        ////////////////
 
-                var MAX_WIDTH = 1920;
-                var MAX_HEIGHT = 1920;
-                var tempW = tempImg.width;
-                var tempH = tempImg.height;
-                if (tempW > tempH) {
-                    if (tempW > MAX_WIDTH) {
-                        tempH *= MAX_WIDTH / tempW;
-                        tempW = MAX_WIDTH;
+        function activate() {
+        }
+
+        function uploadFile(file) {
+
+            if(!file || vm.states.busy) return;
+
+            vm.states.busy = true;
+            vm.states.uploading = false;
+            vm.uploadProgress = 0;
+            vm.states.processing = true;
+
+            vm.resizeImage(file, function(dataURL) {
+                vm.states.processing = false;
+                vm.states.uploading = true;
+
+                console.log("Image data", dataURL);
+
+                file.upload = Upload.http({
+                    url: 'https://api.imgur.com/3/image',
+                    method: 'POST',
+                    headers: {
+                        //'Content-Type': file.type,
+                        Accept: 'application/json',
+                        'Authorization': "Client-ID bc7544395115c3b"
+                    },
+                    data: {
+                        image: dataURL.split(',')[1],
+                        type: "base64"
                     }
-                } else {
-                    if (tempH > MAX_HEIGHT) {
-                        tempW *= MAX_HEIGHT / tempH;
-                        tempH = MAX_HEIGHT;
+                });
+
+                file.upload.then(function (response) {
+
+                    // TODO: Check if successfull response
+
+                    console.log("Succes??", response);
+
+                    vm.states.uploading = false;
+                    vm.states.processing = true;
+                    vm.uploadUrl(response.data.data.link);
+
+                    //file.result = response.data;
+                }, function (response) {
+                    if (response.status > 0) {
+                        console.log("Something went wrong :(");
                     }
+                });
+
+                file.upload.progress(function (evt) {
+                    file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+                    vm.uploadProgress = file.progress;
+                    if(file.progress == 100) {
+                        vm.states.uploading = false;
+                        vm.states.processing = true;
+                    }
+                    console.log("Progress underway!", file.progress);
+                });
+
+            });
+
+        }
+
+        function resizeImage(file, callback) {
+
+            console.log("Provided file", file);
+
+            var reader = new FileReader();
+            reader.onloadend = function() {
+
+                var tempImg = new Image();
+                tempImg.src = reader.result;
+                tempImg.onload = function() {
+
+                    var MAX_WIDTH = 1920;
+                    var MAX_HEIGHT = 1920;
+                    var tempW = tempImg.width;
+                    var tempH = tempImg.height;
+                    if (tempW > tempH) {
+                        if (tempW > MAX_WIDTH) {
+                            tempH *= MAX_WIDTH / tempW;
+                            tempW = MAX_WIDTH;
+                        }
+                    } else {
+                        if (tempH > MAX_HEIGHT) {
+                            tempW *= MAX_HEIGHT / tempH;
+                            tempH = MAX_HEIGHT;
+                        }
+                    }
+
+                    var canvas = document.createElement('canvas');
+                    canvas.width = tempW;
+                    canvas.height = tempH;
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(this, 0, 0, tempW, tempH);
+                    var dataURL = canvas.toDataURL("image/jpeg");
+                    var data = 'image=' + dataURL;
+
+                    callback(dataURL);
+
+
                 }
 
-                var canvas = document.createElement('canvas');
-                canvas.width = tempW;
-                canvas.height = tempH;
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(this, 0, 0, tempW, tempH);
-                var dataURL = canvas.toDataURL("image/jpeg");
-                var data = 'image=' + dataURL;
-
-                console.log("Image data", data, dataURL);
-
             }
-
-        }
-        reader.readAsDataURL(file);
-    }
-
-    imageUpload.uploadFile = function(file) {
-
-        if(!file || imageUpload.states.busy) return;
-
-        imageUpload.states.busy = true;
-        imageUpload.states.uploading = true;
-        imageUpload.uploadProgress = 0;
-        imageUpload.states.processing = false;
-
-        imageUpload.resizeImage(file);
-
-        /**
-
-        file.upload = Upload.http({
-            url: 'https://api.imgur.com/3/image',
-            method: 'POST',
-            headers: {
-                'Content-Type': file.type,
-                'Authorization': "Client-ID bc7544395115c3b"
-            },
-            data: file
-        });
-
-        file.upload.then(function (response) {
-
-            // TODO: Check if successfull response
-
-            console.log("Succes??", response);
-
-            imageUpload.states.uploading = false;
-            imageUpload.states.processing = true;
-            imageUpload.uploadUrl(response.data.data.link);
-
-            //file.result = response.data;
-        }, function (response) {
-            if (response.status > 0) {
-                console.log("Something went wrong :(");
-            }
-        });
-
-        file.upload.progress(function (evt) {
-            file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-            imageUpload.uploadProgress = file.progress;
-            if(file.progress == 100) {
-                imageUpload.states.uploading = false;
-                imageUpload.states.processing = true;
-            }
-            console.log("Progress underway!", file.progress);
-        });
-
-        **/
-
-    }
-
-    imageUpload.uploadUrlCheck = function(formValid, url) {
-        if(!formValid) {
-            console.log("Form is invalid");
-            return;
+            reader.readAsDataURL(file);
         }
 
-        if(imageUpload.states.busy) return;
-        imageUpload.states.busy = true;
+        function uploadUrlCheck(formValid, url) {
+            if(!formValid) {
+                console.log("Form is invalid");
+                return;
+            }
 
-        imageUpload.uploadUrl(url);
-    }
+            if(vm.states.busy) return;
+            vm.states.busy = true;
 
-    imageUpload.uploadUrl = function(url) {
-        imageUpload.states.processing = true;
-
-        var sendData = {
-            original : null,
-            remote_url : url
+            vm.uploadUrl(url);
         }
 
-        StoryService.UploadImage(sendData).then(function(data) {
+        function uploadUrl(url) {
+            vm.states.processing = true;
 
-            // TODO: Check if successful response
-            if(data.success == false) {
-                console.log("Error: " + data.message);
+            var sendData = {
+                original : null,
+                remote_url : url
+            }
+
+            StoryService.UploadImage(sendData).then(function(data) {
+
+                // TODO: Check if successful response
+                if(data.success == false) {
+                    console.log("Error: " + data.message);
+                } else {
+                    console.log("Successfully sent to Heroku!", data);
+                    vm.uploadedImage = data.data;
+                    vm.updateImage();
+                }
+            }, function(error) {
+                console.log("Error: " + error);
+            });
+        }
+
+        function updateImage() {
+            vm.states.uploading = false;
+            vm.states.processing = false;
+            vm.states.busy = false;
+            vm.image = vm.uploadedImage.remote_url;
+        }
+
+        function confirm() {
+            console.log("Finished on this page!", vm.uploadedImage);
+            if(vm.uploadedImage != null) {
+                $scope.closeThisDialog(vm.uploadedImage);
             } else {
-                console.log("Successfully sent to Heroku!", data);
-                imageUpload.uploadedImage = data.data;
-                imageUpload.updateImage();
+                $scope.closeThisDialog();
             }
-        }, function(error) {
-            console.log("Error: " + error);
-        });
-    }
+        }
 
-    imageUpload.updateImage = function() {
-        imageUpload.states.uploading = false;
-        imageUpload.states.processing = false;
-        imageUpload.states.busy = false;
-        imageUpload.image = imageUpload.uploadedImage.remote_url;
-    }
-
-    //imageUpload.uploadFile = function(file) {
-    //
-    //    Upload.dataUrl(file).then(function(url) {
-    //       console.log("Converted file!", url);
-    //    });
-    //
-    //    console.log("Sending this data for upload", file);
-    //    ImgurService.UploadImage(file)
-    //        .then(function(data) {
-    //            if(data.success) {
-    //                console.log("Successfully loaded story data", data);
-    //            } else {
-    //                console.log("Failed to load", data);
-    //            }
-    //        }, function(error) {
-    //            console.log("Error: " + error);
-    //        })
-    //}
-
-    imageUpload.confirm = function() {
-        console.log("Finished on this page!", imageUpload.uploadedImage);
-        if(imageUpload.uploadedImage != null) {
-            $scope.closeThisDialog(imageUpload.uploadedImage);
-        } else {
+        function cancel() {
             $scope.closeThisDialog();
         }
+
+
     }
 
-    imageUpload.cancel = function() {
-        $scope.closeThisDialog();
-    }
-
-}]);
+})();
 
